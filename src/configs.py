b7bd499d37952d84d2aad752bb40ed10c00a37b4
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-07-10 10:48:09 -0300

    update version

--- src/configs.py
+++ src/configs.py
@@ -3,7 +3,7 @@
 
 app_name = "FT3D++"
 app_description = "FT3D rewrite"
-app_version = "0.3.0"
+app_version = "0.4.0"
 app_copyright = "Copyright 2026 Omar Zagonel El Laden"
 app_license = "License: GPLv3"
 
