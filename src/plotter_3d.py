6a945d25daa8ac77b3fb5909eeb1f6d699df1c21
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-07-11 22:32:42 -0300

    add spinbox to change 3D resolutions

--- src/plotter_3d.py
+++ src/plotter_3d.py
@@ -6,27 +6,106 @@ from matplotlib.backends.backend_tkagg import (
 )
 from matplotlib import cm
 
+from configs import *
+
 
 class Plotter3D:
     def __init__(self, parent, app):
         self.app = app
 
-        self.window = tk.Toplevel(parent)
-        self.window.title("Gráfico 3D")
-        self.window.transient(parent)
+        self.tl = tk.Toplevel(parent)
+        self.tl.title("Gráfico 3D")
+        self.tl.transient(parent)
 
         # Blank Figure
         self.fig = Figure(figsize=(6, 5), dpi=100)
         self.ax = self.fig.add_subplot(111, projection="3d")
         self.ax.view_init(elev=30, azim=45)
 
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.tl)
         self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self.window)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.tl)
         self.toolbar.update()
 
+
+        self.init_resolution = 70
+        self.max_resolution = 150
+        self.app.math_utils.resolution_u = self.init_resolution
+        self.app.math_utils.resolution_v = self.init_resolution
+
         self._update_curve()
+
+
+        # Buttons
+
+        self.fr_bt = tk.Frame(self.tl, bg=color_bg, pady=10)
+        self.fr_bt.pack(fill="x")
+
+        self.box_res_u = self._create_spinbox("U", self._change_resolution_u)
+        self.box_res_v = self._create_spinbox("V", self._change_resolution_v)
+
+
+    def _create_spinbox(self, axis, cmd):
+        fr_padx = 2
+
+        tk.Label(
+            self.fr_bt,
+            text="pts",
+            fg=color_text,
+            bg=color_bg
+        ).pack(side="right", padx=fr_padx)
+
+        spinbox = tk.Spinbox(
+            self.fr_bt,
+            from_=5, to=self.max_resolution,
+            increment=5,
+            width=4,
+            textvariable=tk.DoubleVar(value=self.init_resolution),
+            bg=color_bg_spin,
+            fg=color_text,
+            insertbackground=color_text,
+            buttonbackground=color_bg,
+            command=cmd
+        )
+        spinbox.pack(side="right", padx=fr_padx)
+
+        tk.Label(
+            self.fr_bt,
+            text=f"Resolução {axis} =",
+            fg=color_text,
+            bg=color_bg
+        ).pack(side="right", padx=(25, fr_padx))
+
+        # Events to confirm keyboard values
+        spinbox.bind("<Return>",   lambda event: cmd())
+        spinbox.bind("<FocusOut>", lambda event: cmd())
+
+        return spinbox
+
+    def _change_resolution_u(self):
+        resolution = self.box_res_u.get()
+        if (resolution.isdigit() and
+            int(resolution) > 1 and
+            int(resolution) <= self.max_resolution
+        ):
+            self.app.math_utils.resolution_u = int(self.box_res_u.get())
+            self._update_curve()
+        else:
+            self.box_res_u.delete(0, "end")
+            self.box_res_u.insert(0, str(self.app.math_utils.resolution_u))
+
+    def _change_resolution_v(self):
+        resolution = self.box_res_v.get()
+        if (resolution.isdigit() and
+            int(resolution) > 1 and
+            int(resolution) <= self.max_resolution
+        ):
+            self.app.math_utils.resolution_v = int(self.box_res_v.get())
+            self._update_curve()
+        else:
+            self.box_res_v.delete(0, "end")
+            self.box_res_v.insert(0, str(self.app.math_utils.resolution_v))
 
     def _update_curve(self):
         self.ax.clear()
